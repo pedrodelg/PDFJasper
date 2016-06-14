29a25649d03f32941a5499391b602a67c7a50bf0
PedroDelg <pedro7jd@gmail.com>
2016-06-14 14:22:13 -0400

    Genera el reporte bien, poner el original

---
+++ WebContent/jsp/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Generate PDF</title>
+</head>
+<body>
+<h1>Generar Reporte</h1>
+<br>
+	<form action="http://localhost:8090/GenerarPdf/ReporteServlet"> <!-- cambiar la direccion del servidor  --> 
+		<input type="submit" value="Generate PDF report">
+	</form>
+
+</body>
+</html> 
---
+++ src/com/pedroDelg/reporte/GenerarPdf.java
@@ -0,0 +1,43 @@
+package com.pedroDelg.reporte;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+
+public class GenerarPdf {
+
+	public static void createPDF(String file) {
+		
+        try
+	        {
+        	
+       	 
+	            JasperReport jasperReport = null;
+	            JasperPrint jasperPrint = null;
+	            JasperDesign jasperDesign = null;
+	            Map parameters = new HashMap();
+	            jasperDesign = JRXmlLoader.load("D:/Practicas Java/GenerarPdf/WebContent/report1.jrxml"); //direccion del archivo jrxml
+	            jasperReport = JasperCompileManager.compileReport(jasperDesign);  //complia el archivo
+	            jasperPrint  = JasperFillManager.fillReport(jasperReport, parameters, new JRBeanCollectionDataSource(com.pedroDelg.reporte.Student.getStudentList()));
+	            JasperExportManager.exportReportToPdfFile(jasperPrint,file); //crea el pdf y lo manda a la carpeta temporal  (el parametro file es el q contiene esa ruta)
+//	            JasperViewer.viewReport(jasperPrint); esto no se usa, solo para hacer pruebas de visualizacion de reporte con el jasperviewer 
+	            
+	    
+
+	        }
+	        catch(Exception ex)
+	        {
+	            System.out.println("EXCEPTION: "+ex);
+	        }
+		
+		
+	}
+}
